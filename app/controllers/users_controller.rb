53142e64bb0568685b7b4968862706304520a08f
likapantrip <likapantrip@gmail.com>
2024-11-15 19:20:49 +0900

    ルーティングの見直し

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-    @countries_statuses = CountryStatus.includes(:user, :country).where(user_id: params[:id])
+  def index
+    if user_signed_in?
+      @user = current_user
+      if CountryStatus.find_by(user_id: @user.id).nil?
+        count = Country.count
+        count.times do |c|
+          CountryStatus.create(
+            user_id: @user.id,
+            country_id: c+1,
+            status_id: 40
+          )
+        end
+      end
+      redirect_to user_countries_path(@user.id)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'countries#index'
-  resources :countries, only: :index
-  resources :users, only: :show
+  root to: 'users#index'
+  resources :users, only: :index do
+    resources :countries, only: :index
+  end
 end
