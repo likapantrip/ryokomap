867157ea436b0231e6b256314af19ecf4c983f79
likapantrip <likapantrip@gmail.com>
2024-11-18 18:44:17 +0900

    国編集機能を追加

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,6 +1,39 @@
 class CountriesController < ApplicationController
   def index
+    get_countries
+  end
+
+  def edit
+    get_countries
+  end
+
+  def update
+    # ユーザーのステータス件数を取得
+    get_countries
+    count = @countries.count
+    
+    count.times do |c|
+      # 更新対象+パラメーターから値を取得
+      c += 1
+      country_id = c.to_s
+      params_country = params.require(:"country_status").require(:"countries").require(country_id).permit(:status_id)
+      country_data = CountryStatus.find_by(user_id: params[:user_id], country_id: country_id)
+      binding.pry
+
+      # データを更新
+      if country_data.update(params_country) == false
+        flash[:alert] = "データ更新が失敗しました"
+        redirect_to user_edit_country_path
+        return
+      end
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def get_countries
     @user = User.find(params[:user_id])
-    @countries_statuses = CountryStatus.includes(:user, :country).where(user_id: params[:user_id])
+    @countries = CountryStatus.includes(:user, :country).where(user_id: params[:user_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root to: 'users#index'
   resources :users, only: :index do
     resources :countries, only: :index
+    get '/countries/edit', to: 'countries#edit', as: 'edit_country'
+    patch '/countries', to: 'countries#update', as: 'country'
   end
 end
